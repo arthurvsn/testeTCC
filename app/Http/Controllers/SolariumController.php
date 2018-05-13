24bcb890c91b4646d68d369950189ca9d303095f
arthurvsn <arthurvsn@gmail.com>
2018-05-13 15:25:56 -0300

    FOrmaatacao do objeto de resposta

--- app/Http/Controllers/SolariumController.php
+++ app/Http/Controllers/SolariumController.php
@@ -1,15 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use \App\Response\Response;
 
 class SolariumController extends Controller
 {
     protected $client;
+    protected $response;
 
     public function __construct(\Solarium\Client $client)
     {
         $this->client = $client;
+        $this->response = new Response();
     }
 
     public function ping()
@@ -26,15 +29,46 @@ class SolariumController extends Controller
         }
     }
 
+    /**
+     * Function to search in a documents
+     * @param string $stringSearch
+     * @return Response
+     */
     public function search($stringSearch)
     {
+        
         $query = $this->client->createSelect();
         $query->createFilterQuery('abreviacao')->setQuery($stringSearch);
-        $resulset = $this->client->select($query);
+        $resultset = $this->client->select($query);
 
-        // display the total number of documents found by solr
-        echo "<pre>";
-        var_dump($resulset); die();
+        if($resultset->getNumFound() > 0)
+        {
+            $this->response->setType("S");
+            $this->response->setMessages("Elemento founded!");
+
+            foreach ($resultset as $document) 
+            {
+                $fields[] = [
+                    'id' => $document->id,
+                    'nome'=> $document->nome,
+                    'abreviacao'=> $document->abreviacao,
+                    'pais'=> $document->pais,
+                ];
+            }
+            
+            $this->response->setDataSet("Element", $fields);
+        }
+        else 
+        {
+            $this->response->setType("N");
+            $this->response->setMessages("Not Found!");
+        }
+        
+        return response()->json($this->response->toString());
+    }
+
+    public function addDocument() 
+    {
 
     }
 }
